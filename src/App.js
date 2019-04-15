499cfa40a4cc58480b416bc69a43ee585b17f4c6
John Pangalos <johnpa@HIQGBGL0721.local>
2019-04-15 11:12:13 +0200

    Connecting netlify
    - Putting a basic app up and connecting netlify

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
-
-export default App;
+export default () => (
+  <div className="flex flex-col justify-center items-center h-screen">
+    <div className="
+      flex
+      flex-col
+      justify-center
+      items-center
+      bg-grey-lighter
+      px-10
+      py-8
+      rounded-lg
+      text-grey-darkest">
+      <div className="text-3xl font-bold pb-2">App under construction...</div>
+      <div className="text-xl">Please comeback later to see more!</div>
+    </div>
+  </div>
+);
