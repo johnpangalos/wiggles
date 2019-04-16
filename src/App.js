e1b89277726ed1d185a289e2daae2c51312f962c
John Pangalos <john.pangalos@hiq.se>
2019-04-16 11:36:07 +0200

    Getting logout to work
    - Breaking things up a bit
    - Added react router with protected routes
    - Added a login modal
    - Cleaned up an old test
    - Added a silversearch ignore file for convenience

--- src/App.js
+++ src/App.js
@@ -1,65 +1,87 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+
 import './styles/loading-spinner.css';
-
-const getFirebaseToken = () => {
-  window.firebase.auth().useDeviceLanguage();
-  const provider = new window.firebase.auth.GoogleAuthProvider();
-  window.firebase.auth().signInWithRedirect(provider);
-};
-
-const firebaseResponseListener = async setLoading => {
-  try {
-    if (window.localStorage.getItem('token')) return setLoading(false);
-    const result = await window.firebase.auth().getRedirectResult();
-    if (result.credential) {
-      window.localStorage.setItem('token', result.credential.accessToken);
-      window.localStorage.setItem('user', JSON.stringify(result.user));
-      return;
-    }
-    getFirebaseToken();
-  } catch (error) {
-    console.error(error);
-    getFirebaseToken();
-  }
-};
+import { Login } from './Login';
+import { Loading } from './Loading';
 
 export default () => {
+  const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    firebaseResponseListener(setLoading);
+    window.firebase.auth().onAuthStateChanged(res => {
+      if (res) setUser(res);
+      setLoading(false);
+    });
   });
+
+  const signOut = () => {
+    window.firebase.auth().signOut();
+    setUser(null);
+  };
 
   return (
     <div
       id="App"
-      className="flex flex-col justify-center items-center h-screen"
+      className="flex flex-col h-screen w-full items-center text-grey-darkest"
     >
-      {loading ? <Loading /> : <ConstructionMessage />}
+      <Router>
+        <div className="flex items-center flex-shrink bg-grey-lighter w-full h-12 shadow">
+          <div className="flex-grow" />
+          <div onClick={() => signOut()} className="pr-5">
+            {user && (
+              <button className="bg-transparent hover:bg-grey-light py-2 px-4">
+                Logout
+              </button>
+            )}
+          </div>
+        </div>
+        <div className="flex items-center flex-grow w-full">
+          {loading ? (
+            <Loading />
+          ) : (
+            <div className="h-full w-full">
+              <PrivateRoute
+                exact
+                user={user}
+                path="/"
+                component={ConstructionMessage}
+              />
+              <Route path="/login" component={() => <Login user={user} />} />
+            </div>
+          )}
+        </div>
+      </Router>
     </div>
   );
 };
 
-const Loading = () => (
-  <div className="loading">
-    <div />
+const ConstructionMessage = () => (
+  <div className="flex flex-col justify-center items-center h-full">
+    <div className="bg-grey-lighter px-10 py-8 self-center rounded-lg">
+      <div className="text-3xl font-bold pb-2">App under construction...</div>
+      <div className="text-xl">Please comeback later to see more!</div>
+    </div>
   </div>
 );
 
-const ConstructionMessage = () => (
-  <div
-    className="
-      flex
-      flex-col
-      justify-center
-      items-center
-      bg-grey-lighter
-      px-10
-      py-8
-      rounded-lg
-      text-grey-darkest"
-  >
-    <div className="text-3xl font-bold pb-2">App under construction...</div>
-    <div className="text-xl">Please comeback later to see more!</div>
-  </div>
-);
+const PrivateRoute = ({ component: Component, user, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        user ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  );
+};
---
+++ src/Login.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { Redirect } from 'react-router-dom';
+
+const getFirebaseToken = () => {
+  window.firebase.auth().useDeviceLanguage();
+  const provider = new window.firebase.auth.GoogleAuthProvider();
+  window.firebase.auth().signInWithRedirect(provider);
+};
+
+export const Login = ({ user }) => {
+  return user ? <Redirect to="/" /> : <LoginModal />;
+};
+
+const LoginModal = () => (
+  <div className="flex justify-center items-center h-full w-full">
+    <div className="flex flex-col bg-grey-lightest p-4 sm:w-64 w-full shadow">
+      <div className="text-xl pb-2">Please Login</div>
+      <button
+        onClick={() => getFirebaseToken()}
+        className="flex items-baseline bg-red py-2 text-white"
+      >
+        <div className="px-3">
+          <FontAwesomeIcon color="white" icon={faGoogle} />
+        </div>
+        <div>Login with Google</div>
+      </button>
+    </div>
+  </div>
+);
