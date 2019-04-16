95899203541a5a1697db30072c33dde84f1e56c4
John Pangalos <john.pangalos@hiq.se>
2019-04-16 13:21:40 +0200

    Fixing the icons
    - Making the icons load from the correct registry
    - Upgrading node in travis

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,8 @@
-import React from 'react';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 import { Redirect } from 'react-router-dom';
+import { Loading } from './Loading';
 
 const getFirebaseToken = () => {
   window.firebase.auth().useDeviceLanguage();
@@ -13,20 +14,29 @@ export const Login = ({ user }) => {
   return user ? <Redirect to="/" /> : <LoginModal />;
 };
 
-const LoginModal = () => (
-  <div className="flex justify-center items-center h-full w-full">
-    <div className="flex flex-col bg-grey-lightest p-4 sm:w-64 w-full shadow">
-      <div className="text-xl pb-2">Please Login</div>
-      <button
-        onClick={() => getFirebaseToken()}
-        className="flex items-baseline bg-red py-2 text-white"
-      >
-        <div>Login with Google</div>
-      </button>
-    </div>
-  </div>
-);
+const LoginModal = () => {
+  const [loading, setLoading] = useState(false);
+  const handleClick = () => {
+    setLoading(true);
+    getFirebaseToken();
+  };
 
-// <div className="px-3">
-// <FontAwesomeIcon color="white" icon={faGoogle} />
-// </div>
+  return loading ? (
+    <Loading />
+  ) : (
+    <div className="flex justify-center items-center h-full w-full">
+      <div className="flex flex-col bg-grey-lightest p-4 sm:w-64 w-full shadow">
+        <div className="text-xl pb-2">Please Login</div>
+        <button
+          onClick={() => handleClick()}
+          className="flex items-baseline bg-red py-2 text-white"
+        >
+          <div className="px-3">
+            <FontAwesomeIcon color="white" icon={faGoogle} />
+          </div>
+          <div>Login with Google</div>
+        </button>
+      </div>
+    </div>
+  );
+};
