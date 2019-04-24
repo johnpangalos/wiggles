92002d90fe4b6dc69ac715363fb281cb068d6a38
John Pangalos <john.pangalos@hiq.se>
2019-04-24 13:46:00 +0200

    Add the feed
    Connecting to the data base and loading all images
    
    - These should be lazy loaded eventually

--- src/containers/Feed.js
+++ src/containers/Feed.js
@@ -1,13 +1,57 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export const Feed = () => {
-  const getImages = async () => {
-    const imagesRef = window.firebase.database().ref('images/');
-    const snapshot = await imagesRef.once('value');
-    console.log(snapshot.val());
-  };
+  const [images, setImages] = useState({});
+  const callback = snapshot => setImages({ ...images, ...snapshot.val() });
+
   useEffect(() => {
-    getImages();
+    const imagesRef = window.firebase.database().ref('images/');
+    imagesRef.on('value', callback);
+    return () => {
+      imagesRef.off('value', callback);
+    };
   }, []);
-  return <div>Cool stuff!</div>;
+
+  return (
+    <div className="h-full pb-16">
+      <div className="h-full overflow-y-scroll">
+        <div className="p-8">
+          {Object.keys(images).length > 0 &&
+            Object.values(images)
+              .sort((a, b) => (a.timestamp - b.timestamp) * -1)
+              .map(image => <Image key={image.id} image={image} />)}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Image = ({ image }) => {
+  const [imageUrl, setImageUrl] = useState(null);
+
+  const getImageUrl = async () => {
+    var storage = window.firebase.storage();
+    const url = await storage.ref(image.path).getDownloadURL();
+    setImageUrl(url);
+  };
+
+  useEffect(() => {
+    getImageUrl();
+  }, []);
+
+  return (
+    <div className="flex items-center justify-center py-5">
+      <div
+        className="bg-grey-light"
+        style={{
+          height: '500px',
+          width: '500px'
+        }}
+      >
+        <div className="flex justify-center items-center h-full w-full">
+          <img className="max-h-full" src={imageUrl} alt={image.path} />
+        </div>
+      </div>
+    </div>
+  );
 };
