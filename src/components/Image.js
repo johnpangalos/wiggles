a7c0e1b50f12ecfd7080885f9e7b989f84f71882
John Pangalos <john.pangalos@hiq.se>
2019-05-09 10:16:22 +0200

    Add user and date to uploaded images
    - Added the ability to see the user who uploaded an image

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,8 +1,10 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { Fade } from '~/components/transitions';
+import { ProfileImage } from '~/components';
 
 export const Image = ({
   url,
+  timestamp = 0,
   index,
   size,
   lazyLoadStart = 2,
@@ -10,7 +12,9 @@ export const Image = ({
   style = {},
   selected = false,
   selectable = false,
-  handleClick = () => null
+  handleClick = () => null,
+  profile = false,
+  account
 }) => {
   const [imageUrl, setImageUrl] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -52,6 +56,7 @@ export const Image = ({
       };
     }
   }, [img]);
+  const date = new Date(Number(timestamp));
 
   return (
     <Fade appear show={!loading}>
@@ -61,12 +66,12 @@ export const Image = ({
         }`}
       >
         <div
-          className={`bg-white shadow-md rounded px-2 pt-3 pb-8 w-full h-${size} max-w-${size}${
+          className={`flex flex-col bg-white shadow-md rounded px-2 pt-3 pb-4 w-full h-${size} max-w-${size}${
             selectable ? ' cursor-pointer' : ''
           }${selected ? ' border-primary border-2' : ''}`}
         >
           <div
-            className="flex bg-secondary-dark p-1 rounded justify-center items-center h-full w-full"
+            className="flex flex-grow bg-secondary-dark p-1 rounded justify-center items-center h-full"
             onClick={() => handleClick()}
           >
             <img
@@ -80,6 +85,17 @@ export const Image = ({
               style={style}
             />
           </div>
+          {account && (
+            <div className="flex pt-3 items-start">
+              <div className="h-10 w-10">
+                <ProfileImage url={account.photoURL} />
+              </div>
+              <div className="pl-3">
+                <div className="text-xl font-bold">{account.displayName}</div>
+                <div className="text-sm">Uploaded: {date.toLocaleString()}</div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </Fade>
--- src/components/ProfileImage.js
+++ src/components/ProfileImage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const ProfileImage = ({ url }) => (
   <div
-    className="w-full h-full bg-contain rounded-full"
+    className="w-full h-full bg-cover rounded-full bg-no-repeat bg-center"
     style={{ backgroundImage: `url(${url})` }}
     alt="Profile Image"
   />
