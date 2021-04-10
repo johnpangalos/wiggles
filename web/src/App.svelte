07264914888d2fb82368e69b1042561e1a75eaa9
John Pangalos <jpangs88@gmail.com>
2021-04-10 09:28:43 +0200

    More work on svelte version

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,7 +1,35 @@
 <script lang="ts">
-  import firebase from "firebase/app";
-  import Counter from "./lib/Counter.svelte";
+  import { getApp } from "firebase/app";
   import { Button, Loading } from "./lib";
+  import {
+    getAuth,
+    getRedirectResult,
+    GoogleAuthProvider,
+  } from "firebase/auth";
+
+  const auth = getAuth(firebaseApp);
+  getRedirectResult(auth)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access Google APIs.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+
+      // The signed-in user info.
+      const user = result.user;
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+
+  const firebaseApp = getApp();
+
   const provider = new firebase.auth.GoogleAuthProvider();
   enum LogInState {
     LoggedIn,
--- web/src/main.ts
+++ web/src/main.ts
@@ -1,21 +1,17 @@
 import App from "./App.svelte";
 import "./index.css";
-export default (async () => {
-  const firebase = await import("firebase/app");
-  await import("firebase/auth");
-  await import("firebase/firestore");
+import { initializeApp } from "firebase/app";
 
-  firebase.default.initializeApp({
-    apiKey: "AIzaSyDcx5xDlQS3ixEFF8mESoxUzTk9f56uQhA",
-    authDomain: "wiggles-f0bd9.firebaseapp.com",
-    databaseURL: "https://wiggles-f0bd9.firebaseio.com",
-    projectId: "wiggles-f0bd9",
-    storageBucket: "wiggles-f0bd9.appspot.com",
-    messagingSenderId: "837754270874",
-    appId: "1:837754270874:web:4760e51978d04dfb",
-  });
+initializeApp({
+  apiKey: "AIzaSyDcx5xDlQS3ixEFF8mESoxUzTk9f56uQhA",
+  authDomain: "wiggles-f0bd9.firebaseapp.com",
+  databaseURL: "https://wiggles-f0bd9.firebaseio.com",
+  projectId: "wiggles-f0bd9",
+  storageBucket: "wiggles-f0bd9.appspot.com",
+  messagingSenderId: "837754270874",
+  appId: "1:837754270874:web:4760e51978d04dfb",
+});
 
-  return new App({
-    target: document.body,
-  });
-})();
+new App({
+  target: document.body,
+});
