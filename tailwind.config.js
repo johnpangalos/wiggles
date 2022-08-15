61ef746826ad5a398c38f9828230eb089476fcb8
John Pangalos <john@pangalos.dev>
2022-08-15 20:33:57 +0200

    update github flows (#137)

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      xs: "340px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+  },
+  variants: {},
+  plugins: [],
+};
