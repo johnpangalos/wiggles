ce2037f0f2e1d5671f80a01080a0ea3df6847fe9
John Pangalos <john@pangalos.dev>
2022-09-27 23:41:28 +0200

    remove pwa

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,34 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { VitePWA } from "vite-plugin-pwa";
-import replace, { RollupReplaceOptions } from "@rollup/plugin-replace";
-
-const replaceOptions: RollupReplaceOptions = {
-  __DATE__: new Date().toISOString(),
-  preventAssignment: true,
-};
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      manifest: false,
-      registerType: "autoUpdate",
-      includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.png"],
-      // switch to "true" to enable sw on development
-      devOptions: {
-        enabled: true,
-      },
-      workbox: {
-        globPatterns: ["**/*.{js,css,html}"],
-      },
-      filename: "sw.js",
-      strategies: "generateSW",
-    }),
-    // @ts-ignore
-    replace(replaceOptions),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
