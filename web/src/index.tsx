fbea412372009b099016f91f6c8e37e95e3d71d3
John Pangalos <john@pangalos.dev>
2026-02-27 06:25:15 +0100

    Replace useMutation with React Router action on Upload page (#220)

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,18 +1,9 @@
 import { createRoot } from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "@/styles/index.css";
 import App from "@/App";
-
-const queryClient = new QueryClient();
 
 const container = document.getElementById("root");
 
 const root = createRoot(container!);
 
-root.render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-    <ReactQueryDevtools initialIsOpen={false} />
-  </QueryClientProvider>,
-);
+root.render(<App />);
